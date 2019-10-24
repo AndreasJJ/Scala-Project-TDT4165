9baa2352ac7fdd1897ce49a1e5813403ef3dd133
Andreasjj <andreasjj@gmail.com>
2019-10-24 15:51:44 +0200

    Implemented project task 2

--- project_tasks/src/main/scala/Bank.scala
+++ project_tasks/src/main/scala/Bank.scala
@@ -1,15 +1,48 @@
+import exceptions.{IllegalAmountException, NoSufficientFundsException}
+
 class Bank(val allowedAttempts: Integer = 3) {
 
     private val transactionsQueue: TransactionQueue = new TransactionQueue()
     private val processedTransactions: TransactionQueue = new TransactionQueue()
 
-    def addTransactionToQueue(from: Account, to: Account, amount: Double): Unit = ???
+    def addTransactionToQueue(from: Account, to: Account, amount: Double): Unit = {
+        var transaction = new Transaction(transactionsQueue, processedTransactions, from, to, amount, 1)
+        this.processedTransactions.push(transaction)
+        this.processTransactions
+    }
                                                 // TODO
                                                 // project task 2
                                                 // create a new transaction object and put it in the queue
                                                 // spawn a thread that calls processTransactions
 
-    private def processTransactions: Unit = ???
+    private def processTransactions: Unit = {
+        if(!transactionsQueue.isEmpty) {
+            var next = transactionsQueue.pop
+            try {
+                next.run()
+                next.status = TransactionStatus.SUCCESS
+            } catch {
+                case illegalAmount: IllegalAmountException => {
+                    next.status = TransactionStatus.FAILED
+                    this.processedTransactions.push(next)
+                }
+                case noSufficientFunds: NoSufficientFundsException => {
+                    next.attempt -= 1
+                    if (next.attempt == 0) {
+                        next.status = TransactionStatus.FAILED
+                    }
+                    else {
+                        this.transactionsQueue.push(next)
+                    }
+                }
+            } finally {
+                if (next.status != TransactionStatus.PENDING) {
+                    this.processedTransactions.push(next)
+                }  
+            }
+            this.processTransactions
+        }
+    }
                                                 // TOO
                                                 // project task 2
                                                 // Function that pops a transaction from the queue
