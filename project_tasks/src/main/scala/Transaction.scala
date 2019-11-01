c618e8645b5c16c44f365a01fd1d94d851702dcd
MittBrukernavn <mittbrukernavn.aa@gmail.com>
2019-11-01 15:19:05 +0100

    Fixed some weird indenting I didn't see earlier :art:

--- project_tasks/src/main/scala/Transaction.scala
+++ project_tasks/src/main/scala/Transaction.scala
@@ -87,8 +87,8 @@ class Transaction(val transactionsQueue: TransactionQueue,
                     }
                 }
             }
-            Thread.sleep(50) // you might want this to make more room for
-                                                    // new transactions to be added to the queue
+            Thread.sleep(50)    // you might want this to make more room for
+                                // new transactions to be added to the queue
         }
     }
 }
